c48f4aa8a3451ab84611b9dd3ab7a604edd39bae
Horoshko <khorosko15@gmail.com>
2025-03-05 23:52:32 +0300

    Add Mover and Add FileManager

---
+++ LR1/Paint/Actions/FileManager.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+internal class FileManager
+{
+
+   public void InputFile(List<Figure> figures)
+   {
+        var settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            Formatting = Formatting.Indented
+        };
+
+        string json = JsonConvert.SerializeObject(figures, settings);
+        File.WriteAllText("storage.json", json);
+
+   }
+
+   
+   public List<Figure> OutputFile() {
+
+        string json = File.ReadAllText("storage.json");
+
+        var settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto
+        };
+
+
+        List<Figure>? figures = JsonConvert.DeserializeObject<List<Figure>>(json, settings);
+
+        return figures;
+    }
+
+}
---
+++ LR1/Paint/Actions/Mover.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+internal class Mover
+{
+
+    public void Move(Figure figure, int x, int index)
+    {
+        switch (index)
+        {
+            case 0:
+                figure.X -= x;
+                break;
+            case 1:
+                figure.X += x;
+                break;
+            case 2:
+                figure.Y += x;
+                break;
+            case 3:
+                figure.Y += x;
+                break;
+        }
+    }
+
+}
